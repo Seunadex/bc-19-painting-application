9d9a84d65d884e96fcc4d23f0f404c1aa44db79e
Adekunle Seun <oladeleadekunle94@gmail.com>
2017-02-23 15:47:06 +0100

    Uncommented erase.js

--- js/erase.js
+++ js/erase.js
@@ -1,13 +1,12 @@
-//  function erasePart(e){
-//  	ctx.lineTo(e.clientX, e.clientY);
-//  	ctx.lineWidth = newRadius *2;
-// 	ctx.beginPath();
-// 	ctx.strokeStyle = 'white';
-// 	ctx.stroke();
-// 	ctx.arc(e.clientX, e.clientY, radius*5,0, Math.PI *2, false);
-// 	ctx.globalCompositeOperation = "destination-out";
-// 	ctx.fillStyle = 'white';
-// 	ctx.fill();
+ function erasePart(e){
+ 	ctx.lineTo(e.clientX, e.clientY);
+ 	ctx.lineWidth = newRadius *2;
+	ctx.beginPath();
+	ctx.strokeStyle = 'white';
+	ctx.stroke();
+	ctx.arc(e.clientX, e.clientY, radius*5,0, Math.PI *2, false);
+	ctx.globalCompositeOperation = "destination-out";
+	ctx.fillStyle = 'white';
+	ctx.fill();
 	
-	
-// };
+};	
