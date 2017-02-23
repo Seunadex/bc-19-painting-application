469a969489a205dc509995e452b95b00ca7271e5
Adekunle Seun <oladeleadekunle94@gmail.com>
2017-02-23 13:11:36 +0100

    Made some changes to save feature

--- js/saveDrawing.js
+++ js/saveDrawing.js
@@ -4,12 +4,3 @@ save_btn.addEventListener('click', function(e){
 	save_btn.href = dataURL;
 });
 
-
-var erase-btn = document.getElementById('erase');
-erase-btn.addEventListener('click', function(e){
-ctx.beginPath();
-ctx.globalCompositionOperation="destination-out";
-ctx.arc(e.offsetX, e.offsetY, radius*5, Math.PI *2, false);
-ctx.fill();
-
-});
